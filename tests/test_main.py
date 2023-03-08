5dd79274d45f7b62a099c58a615c6f0b6f0d9433
anastasiya <burykh_2000@mail.ru>
2023-03-09 01:50:12 +0300

    add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,36 @@
 import unittest
 from main.main import Node, Stack
 
+
 class TestNode(unittest.TestCase):
-    def test_node_init(self):
-        node1 = Node(10)
-        assert node1.data == 10
-        assert node1.next_node is None
+    def test_Node(self):
+        n1 = Node(5, None)
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n1.next_node, None)
 
     def test_node_next_node(self):
-        node1 = Node(10)
-        node2 = Node(120, node1)
-        assert node2.next_node is node1
+        node1 = Node(10, None)
+        node2 = Node(12, node1)
+        self.assertEqual(node1.next_node, None)
+        self.assertEqual(node2.next_node.data, 10)
 
 
 class TestStack(unittest.TestCase):
+    def test_stack(self):
+        stack = Stack()
+        self.assertEqual(stack.top, None)
+
+
     def test_push(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data1')
+
+    def test_pop(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        data = stack.pop()
+        self.assertEqual(stack.top.data, 'data1')
