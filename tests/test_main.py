e181226e1c49efe99c73d58625f7a6d67d8c611c
anastasiya <burykh_2000@mail.ru>
2023-03-28 23:41:30 +0300

    add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -66,3 +66,24 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(ll.head, None)
         self.assertEqual(ll.tail, None)
 
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        lst = ll.to_list()
+        self.assertEqual(lst[0]['id'], 0)
+        self.assertEqual(lst[1]['id'], 1)
+        self.assertEqual(lst[2]['id'], 2)
+
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(ll.get_data_by_id(1), {'id': 1, 'username': 'lazzy508509'})
+        self.assertEqual(ll.get_data_by_id(2), {'id': 2, 'username': 'mik.roz'})
+        self.assertEqual(ll.get_data_by_id(4), None)
